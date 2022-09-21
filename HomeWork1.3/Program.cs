f487e8c85b9d7b5bdca6f383a7ec7dd4604a618a
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-21 23:05:50 +0300

    Program3: определяет четное или нечетное число введено

---
+++ HomeWork1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+
+if ((x+y)%2 == 0)
+{
+ y = x/2;
+ Console.Write($"Число {x} является ЧЕТНЫМ");
+}
+else
+{
+ Console.Write($"Число {x} является НЕЧЕТНЫМ");
+}
+
