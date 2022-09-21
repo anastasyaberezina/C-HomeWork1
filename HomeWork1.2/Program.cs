e5e5b95336b10b784c0e875f7562f86b612a5146
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-21 20:02:45 +0300

    Program 2: выведение первого числа из трехзначного

---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+int one = 0;
+
+if (x>100)
+{
+ y = x % 100;
+ one = (x - y) / 100;
+ Console.WriteLine($"Первая цифра введенного числа равна {one}");
+}
