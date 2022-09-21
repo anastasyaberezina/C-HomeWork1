3e15ea239ca68b5c6c607ef4c90358c8719dc562
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-22 00:11:45 +0300

    Program4: вывести все числа промежутка от 1 до вводимого числа

---
+++ HomeWork1.4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Не могу решить эту задачу. Не понимаю, куда писать условие N>X>=1.
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int X = 0;
+
+while (X>=1);
+while (X<N)
+if ((N+X)%2 == 0)
+{
+ X = N/2;
+ Console.Write($"{N} + ","");
+}
+X++;
+
